bd6e42f8ac23438c2f84c89bfe78fddf024180b0
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-25 11:10:20 -0300

    [ :loud_sound: ] | create: logger - constructor (app::config)

--- config/logger.go
+++ config/logger.go
@@ -3,6 +3,7 @@ package config
 import (
 	"io"
 	"log"
+	"os"
 )
 
 type Logger struct {
@@ -11,4 +12,17 @@ type Logger struct {
 	warning *log.Logger
 	error   *log.Logger
 	writer  io.Writer
+}
+
+func NewLogger(p string) *Logger {
+	writter := io.Writer(os.Stdout)
+	logger := log.New(writter, p, log.Ldate|log.Ltime)
+
+	return &Logger{
+		debug:   log.New(writter, "[DEBUG] | ", logger.Flags()),
+		info:    log.New(writter, "[INFO] | ", logger.Flags()),
+		warning: log.New(writter, "[WARNING] | ", logger.Flags()),
+		error:   log.New(writter, "[ERROR] | ", logger.Flags()),
+		writer:  writter,
+	}
 }
