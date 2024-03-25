e8f35fb64e491dbf78c95115c091ae6efbd26833
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-25 11:57:46 -0300

    [ :card_file_box: ] | setup: database - sqlite (app::database)

---
+++ config/sqlite.go
@@ -0,0 +1,48 @@
+package config
+
+import (
+	"os"
+
+	"github.com/Samuel-Ricardo/jobileu/schemas"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func InitializeSQLite() (*gorm.DB, error) {
+	logger := NewLogger("SQLite")
+	dbPath := "./db/main.db"
+
+	_, err := os.Stat(dbPath)
+
+	if os.IsNotExist(err) {
+		logger.Info("Database file not found. Creating...")
+
+		err = os.MkdirAll("./db", os.ModePerm)
+		if err != nil {
+			logger.Error("Failed to create database directory")
+			return nil, err
+		}
+
+		file, err := os.Create(dbPath)
+		if err != nil {
+			logger.Error("Failed to create database file")
+			return nil, err
+		}
+
+		file.Close()
+	}
+
+	db, err := gorm.Open(sqlite.Open(dbPath), &gorm.Config{})
+	if err != nil {
+		logger.Error("Failed to connect to database")
+		return nil, err
+	}
+
+	err = db.AutoMigrate(&schemas.Openig{})
+	if err != nil {
+		logger.Error("Failed to migrate database")
+		return nil, err
+	}
+
+	return db, nil
+}
